ccc130d6babd616f3b1e0eefa08648951e570ac7
Hari Jiang <hari.jiang@outlook.com>
2015-10-31 12:38:39 +0800

    add script zerotest

--- setup.py
+++ setup.py
@@ -7,5 +7,6 @@ setup(name='zerotest',
       description='Generator integrated test for web app with zero lines code need to write',
       author='Hari Jiang',
       author_email='hari.jiang@outlook.com',
-      packages=['distutils', 'distutils.command'],
+      scripts=['scripts/zerotest'],
+      packages=['zerotest'],
       )
