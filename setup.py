cd6455540741bdc80dce0f3c30f5bd3d45dd3d0d
Hari Jiang <hari.jiang@outlook.com>
2015-11-09 21:39:10 +0800

    add test class to setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python
 
+import sys
+
 from setuptools import find_packages, setup
+from setuptools.command.test import test
+
+
+class PyTest(test):
+    def finalize_options(self):
+        test.finalize_options(self)
+        self.test_args = ['tests']
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
 
 setup(name='zerotest',
       version='0.0.0',
@@ -8,10 +25,14 @@ setup(name='zerotest',
       author='Hari Jiang',
       author_email='hari.jiang@outlook.com',
       license='MIT',
+      cmdclass={'test': PyTest},
       packages=find_packages(),
       install_requires=[
           'requests>=2.2.1',
           'Werkzeug>=0.10.4',
+      ],
+      tests_require=[
+          'pytest>=2.8.2',
       ],
       entry_points={
           'console_scripts': [
