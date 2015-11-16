9699d04bbbe99fafff023310ea886660f9454c3c
Tyler Kennedy <tk@tkte.ch>
2015-11-16 14:01:27 -0600

    PEP8 generator_helper.py. Fix invalid name generation.

--- zerotest/utils/generator_helper.py
+++ zerotest/utils/generator_helper.py
@@ -1,18 +1,35 @@
-__author__ = 'Hari Jiang'
+#!/usr/bin/env python
+# encoding: utf-8
+import re
+
+INVALID_METHOD_R = re.compile(r'[^a-zA-Z0-9_]')
 
 
 def get_name_from_request(request):
-    return '{}_{}'.format(request.method.lower(), _path_to_func_name(request.path))
+    return '{}_{}'.format(
+        request.method.lower(),
+        _path_to_func_name(request.path)
+    )
 
 
 def _path_to_func_name(path):
-    if path == '/':
-        name = 'root'
-    else:
-        name = path[1:].replace('/', '_').replace('-', '_')
-    return name
+    """
+    Generate a valid python function name for a given request path.
+
+    The valid characters for a Python 2 function name are defined as follows:
+
+    identifier ::=  (letter|"_") (letter | digit | "_")*
+    letter     ::=  lowercase | uppercase
+    lowercase  ::=  "a"..."z"
+    uppercase  ::=  "A"..."Z"
+    digit      ::=  "0"..."9"
+
+    :param path: The HTTP request path string.
+    :type path: unicode
+    :rtype: unicode
+    """
+    return 'root' if path == '/' else INVALID_METHOD_R.sub('_', path)
 
 
 def dict_to_param_style_code(d):
-    source = ", ".join(map(lambda (k, v): "{}={}".format(k, repr(v)), d.items()))
-    return source
+    return ', '.join('{0}={1}'.format(k, repr(v)) for k, v in d.items())
