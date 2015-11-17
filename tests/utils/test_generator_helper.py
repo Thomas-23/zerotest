695d5ad47c79feca296d58e67825041c45e56474
Tyler Kennedy <tk@tkte.ch>
2015-11-17 11:10:04 -0600

    Test to see if invalid URL chars are properly coverted to _.

--- tests/utils/test_generator_helper.py
+++ tests/utils/test_generator_helper.py
@@ -1,6 +1,21 @@
+# encoding: utf-8
 from collections import OrderedDict
 from zerotest.request import Request
-from zerotest.utils.generator_helper import get_name_from_request, dict_to_param_style_code
+from zerotest.utils.generator_helper import (
+    get_name_from_request,
+    dict_to_param_style_code
+)
+
+
+#: Common HTTP request path characters that are invalid when used for
+#: a python function name.
+INVALID_CHARS = (
+    '*',
+    '-',
+    '#',
+    '/',
+    '.'
+)
 
 
 def test_get_name_from_request():
@@ -8,6 +23,16 @@ def test_get_name_from_request():
     assert get_name_from_request(req) == 'get_root'
     req = Request(path='/the/world', method='PUT')
     assert get_name_from_request(req) == 'put_the_world'
+
+    for invalid_char in INVALID_CHARS:
+        assert(
+            get_name_from_request(
+                Request(
+                    path='/bad_{0}_trailing'.format(invalid_char),
+                    method='GET'
+                )
+            ) == 'get_bad_trailing'
+        )
 
 
 def test_dict_to_param_style_code():
