271340b5d6c8127d620a98356497be18f2295e6b
Hari Jiang <hari.jiang@outlook.com>
2015-11-18 12:10:06 +0800

    bump version to 1.0.2, make pypi description display correct

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ class TestCommand(Command):
 
 
 setup(name='zerotest',
-      version='1.0.1',
+      version='1.0.2',
       long_description=long_description,
       description='Capture HTTP request/response and convert to test code.',
       author='Hari Jiang',
