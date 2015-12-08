f47a917c883d5ed63382c31353a45bc028b9657b
Hari Jiang <hari.jiang@outlook.com>
2015-12-08 16:52:40 +0000

    version 1.2.1, more error info on fuzzy matching mode

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ class TestCommand(Command):
 
 
 setup(name='zerotest',
-      version='1.2.0',
+      version='1.2.1',
       long_description=long_description,
       description=description,
       author='Hari Jiang',
